79dff66d9f8cf42d34d18c456a6df7589940a09e
Chatman R. Jr <crjr.code@protonmail.com>
2021-01-21 16:34:29 -0500

    Remove early init and restructure init.

--- init.el
+++ init.el
@@ -25,20 +25,42 @@
 ;;;;
 ;;;; Code:
 
+(require 'preamble
+	 (concat user-emacs-directory "config/preamble.el"))
+(c4/perf)
+(c4/packages-setup)
+(c4/housekeeping)
+
+(require 'base
+	 (concat user-emacs-directory "config/base.el"))
 (c4/user
   "Chatman R. Jr"
   "crjr.code@protonmail.com")
 (c4/ui)
 (c4/theming 'minimal-light)
 (c4/typography "Input" 12)
-(c4/exwm)
 (c4/ux)
-(c4/projects "~/Workbench")
-(c4/git "cr-jr")
-(c4/org "~/Org")
-(c4/code)
+
+(require 'keybindings
+  (concat user-emacs-directory "config/keybindings.el"))
 (c4/keybindings)
 
+(require 'projects
+	 (concat user-emacs-directory "config/projects.el"))
+(c4/projects "~/Workbench")
+(c4/git "cr-jr")
+
+(require 'code
+	 (concat user-emacs-directory "config/code.el"))
+(c4/code)
+
+(require 'org-setup
+	 (concat user-emacs-directory "config/org-setup.el"))
+(c4/org "~/Org")
+
+(require 'ui
+  (concat user-emacs-directory "config/ui.el"))
+(c4/exwm)
 
 (provide 'init)
 ;;;; init.el ends here
